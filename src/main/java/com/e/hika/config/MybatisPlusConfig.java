6480e176729eb45da54d5385217fa6d08dcff37f
baiyingwaming <baiyingwaming@foxmail.com>
2025-07-17 13:59:14 +0800

    20250717

--- src/main/java/com/e/hika/config/MybatisPlusConfig.java
+++ src/main/java/com/e/hika/config/MybatisPlusConfig.java
@@ -5,9 +5,17 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 分页实现效果.
+ */
 @Configuration
 public class MybatisPlusConfig {
 
+    /**
+     * mybatis-plus 分页插件
+     *
+     * @return
+     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
